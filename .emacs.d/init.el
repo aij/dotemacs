b349f5e10f9618de7537fb971e24464cd74738db
Ivan Jager <aij+git@mrph.org>
2019-04-08 09:08:26 -0500

    emacs: Scala-metals now working
    With aij-nixos-config b233caba3ae6b51b8bfad9d042bbd4247d1cfd27

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -233,7 +233,8 @@
 (use-package flycheck
   :init (global-flycheck-mode))
 
-(use-package lsp-mode)
+(use-package lsp-mode
+  :init (setq lsp-prefer-flymake nil))
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode))
