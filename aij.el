744abe025a824dd94ea70e465d73166f1313cfa4
Ivan Jager <aij+git@mrph.org>
2019-07-26 16:19:39 -0500

    Use lsp in scala-mode

--- aij.el
+++ aij.el
@@ -242,10 +242,9 @@
   :init (global-flycheck-mode))
 
 (use-package lsp-mode
-  :init (setq lsp-prefer-flymake nil))
-
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
+  ;; :init (setq lsp-prefer-flymake nil)
+  ;; Enable lsp-scala automatically in scala files
+  :hook (scala-mode . lsp))
 
 (use-package dhall-mode
   :config
